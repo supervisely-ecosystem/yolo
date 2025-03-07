090c70407e891194569bfec0c214d7e1be1f3d68
cxnt <pavelbartsits@gmail.com>
2025-03-07 18:10:21 +0400

    Update config names to reflect YOLO versions v8 - v12 and remove unused methods in YOLOModel

--- supervisely_integration/serve/serve_yolo.py
+++ supervisely_integration/serve/serve_yolo.py
@@ -1,11 +1,9 @@
 import os
-import re
 from threading import Event
 from typing import Any, Dict, Generator, List, Union
 
 import cv2
 import numpy as np
-import yaml
 from ultralytics import YOLO
 
 import supervisely as sly
@@ -24,6 +22,7 @@ class YOLOModel(sly.nn.inference.ObjectDetection):
     def load_model(
         self, model_files: dict, model_info: dict, model_source: str, device: str, runtime: str
     ):
+        self.model_precision = ModelPrecision.FP32
         checkpoint_path = model_files["checkpoint"]
         if self.model_source == ModelSource.PRETRAINED:
             model_meta = model_info.get("meta", {})
@@ -75,12 +74,6 @@ class YOLOModel(sly.nn.inference.ObjectDetection):
                 tags.append(sly.Tag(self._get_confidence_tag_meta(), dto.score))
             label = sly.Label(geometry, obj_class, tags)
         return label
-
-    def node_label_to_point(self, keypoints):
-        n_label2point = {}
-        for node, keypoint in zip(self.nodes_order, keypoints):
-            n_label2point[node] = keypoint
-        return n_label2point
 
     def _to_dto(self, prediction, settings: dict) -> List[Union[PredictionMask, PredictionBBox]]:
         """Converts YOLO Results to a List of Prediction DTOs."""
@@ -154,7 +147,7 @@ class YOLOModel(sly.nn.inference.ObjectDetection):
         )
         n = len(predictions)
         first_benchmark = predictions[0].speed
-        # YOLOv8 returns avg time per image, so we need to multiply it by the number of images
+        # YOLO returns avg time per image, so we need to multiply it by the number of images
         benchmark = {
             "preprocess": first_benchmark["preprocess"] * n,
             "inference": first_benchmark["inference"] * n,
@@ -165,67 +158,6 @@ class YOLOModel(sly.nn.inference.ObjectDetection):
         to_dto_time = timer.get_time()
         benchmark["postprocess"] += to_dto_time
         return predictions, benchmark
-
-    def _load_pytorch(self, weights_path: str):
-        model = YOLO(weights_path)
-        model.to(self.device)
-        return model
-
-    def _load_runtime(self, weights_path: str, format: str, **kwargs):
-        if weights_path.endswith(".pt"):
-            exported_weights_path = weights_path.replace(".pt", f".{format}")
-            if self.model_precision == ModelPrecision.FP16:
-                exported_weights_path = exported_weights_path.replace(
-                    f".{format}", f"_fp16.{format}"
-                )
-            model = None
-            if not os.path.exists(exported_weights_path):
-                sly.logger.info(f"Exporting model to '{format}' format...")
-                if self.gui is not None:
-                    bar = self.gui.download_progress(
-                        message=f"Exporting model to '{format}' format...", total=1
-                    )
-                    self.gui.download_progress.show()
-                model = YOLO(weights_path)
-                model.export(format=format, **kwargs)
-                if self.model_precision == ModelPrecision.FP16:
-                    # rename after YOLO's export
-                    os.rename(weights_path.replace(".pt", f".{format}"), exported_weights_path)
-                    sly.fs.silent_remove(weights_path.replace(".pt", f".onnx"))
-                if self.gui is not None:
-                    bar.update(1)
-                    self.gui.download_progress.hide()
-            checkpoint_info_path = os.path.join(
-                os.path.dirname(weights_path), "checkpoint_info.yaml"
-            )
-            if self.model_source == ModelSource.CUSTOM and not os.path.exists(checkpoint_info_path):
-                # save custom checkpoint_info in yaml, as it will be lost after exporting
-                if model is None:
-                    model = YOLO(weights_path)
-                self._dump_yaml_checkpoint_info(model, os.path.dirname(weights_path))
-        else:
-            exported_weights_path = weights_path
-
-        task_type_map = {
-            TaskType.OBJECT_DETECTION: "detect",
-            TaskType.INSTANCE_SEGMENTATION: "segment",
-            TaskType.POSE_ESTIMATION: "pose",
-        }
-
-        model = YOLO(exported_weights_path, task=task_type_map[self.task_type])
-        return model
-
-    def _load_onnx(self, weights_path: str):
-        return self._load_runtime(weights_path, "onnx", dynamic=True)
-
-    def _load_tensorrt(self, weights_path: str):
-        return self._load_runtime(
-            weights_path,
-            "engine",
-            dynamic=False,  # batch size is 1
-            # batch=self.TENSORRT_MAX_BATCH_SIZE,
-            half=self.model_precision == ModelPrecision.FP16,
-        )
 
     def _check_onnx_device(self, device: str):
         import onnxruntime as ort
@@ -240,58 +172,45 @@ class YOLOModel(sly.nn.inference.ObjectDetection):
 
     def _check_tensorrt_device(self, device: str):
         if "cuda" not in device:
-            raise ValueError(f"Selected {device} device, but TensorRT only supports CUDA devices")
+            raise ValueError(f"Selected '{device}' device, but TensorRT only supports CUDA devices")
 
-    def _try_extract_checkpoint_info(self, model: YOLO):
-        model_name = None
-        architecture = None
-        # 1. Extract from checkpoint_info.yaml
-        try:
-            yaml_path = os.path.join(self.model_dir, "checkpoint_info.yaml")
-            if os.path.exists(yaml_path):
-                with open(yaml_path, "r") as f:
-                    info = yaml.safe_load(f)
-                model_name = info["model_name"]
-                architecture = info["architecture"]
-                return model_name, architecture
-        except Exception as e:
-            pass
+    def _load_pytorch(self, weights_path: str):
+        model = YOLO(weights_path)
+        model.to(self.device)
+        return model
 
-        # 2. Extract from sly_metadata for custom checkpoints
-        try:
-            model_name = model.ckpt["sly_metadata"]["model_name"]
-            architecture = get_arch_from_model_name(model_name)
-            assert architecture is not None
-            return model_name, architecture
-        except Exception as e:
-            pass
+    def _load_onnx(self, weights_path: str):
+        return self._load_runtime(weights_path, "onnx", dynamic=True)
 
-        # 3. Fallback: trying to extract from train_args for old custom checkpoints
-        try:
-            train_args = model.ckpt["train_args"]
-            model_name = os.path.basename(train_args["model"]).split(".")[0]
-            if "yolov" in model_name:
-                model_name = model_name.replace("yolov", "YOLOv")
-            architecture = get_arch_from_model_name(model_name)
-            assert architecture is not None
-            return model_name, architecture
-        except Exception as e:
-            sly.logger.warn(
-                f"Failed to extract model_name and architecture from train_args. {repr(e)}",
-                exc_info=True,
-            )
-        return model_name, architecture
+    def _load_tensorrt(self, weights_path: str):
+        return self._load_runtime(weights_path, "engine", dynamic=False)
 
-    def _dump_yaml_checkpoint_info(self, model: YOLO, dir_path: str):
-        model_name, architecture = self._try_extract_checkpoint_info(model)
-        info = {
-            "model_name": model_name,
-            "architecture": architecture,
+    def _load_runtime(self, weights_path: str, format: str, **kwargs):
+        if weights_path.endswith(".pt"):
+            exported_weights_path = weights_path.replace(".pt", f".{format}")
+            model = None
+            if not os.path.exists(exported_weights_path):
+                sly.logger.info(f"Exporting model to '{format}' format...")
+                if self.gui is not None:
+                    bar = self.gui.download_progress(
+                        message=f"Exporting model to '{format}' format...", total=1
+                    )
+                    self.gui.download_progress.show()
+                model = YOLO(weights_path)
+                model.export(format=format, **kwargs)
+                if self.gui is not None:
+                    bar.update(1)
+                    self.gui.download_progress.hide()
+        else:
+            exported_weights_path = weights_path
+
+        task_type_map = {
+            TaskType.OBJECT_DETECTION: "detect",
+            TaskType.INSTANCE_SEGMENTATION: "segment",
         }
-        yaml_path = os.path.join(dir_path, "checkpoint_info.yaml")
-        with open(yaml_path, "w") as f:
-            yaml.dump(info, f)
-        return yaml_path
+
+        model = YOLO(exported_weights_path, task=task_type_map[self.task_type])
+        return model
 
     def _load_model_meta(self):
         self.class_names = list(self.model.names.values())
@@ -314,35 +233,4 @@ class YOLOModel(sly.nn.inference.ObjectDetection):
         info["videos_support"] = True
         info["async_video_inference_support"] = True
         info["tracking_on_videos_support"] = True
-        if self.task_type == TaskType.POSE_ESTIMATION:
-            info["detector_included"] = True
         return info
-
-
-def parse_model_name(checkpoint_name: str):
-    # yolov8n
-    if "11" in checkpoint_name:
-        p = r"yolo(\d+)(\w)"
-    else:
-        p = r"yolov(\d+)(\w)"
-    match = re.match(p, checkpoint_name.lower())
-    version = match.group(1)
-    variant = match.group(2)
-    if "11" in checkpoint_name:
-        model_name = f"YOLO{version}{variant}"
-        architecture = f"YOLO{version}"
-    else:
-        model_name = f"YOLOv{version}{variant}"
-        architecture = f"YOLOv{version}"
-    return model_name, architecture
-
-
-def get_arch_from_model_name(model_name: str):
-    # yolov8n-det
-    if "11" in model_name:
-        p = r"yolo(\d+)"
-    else:
-        p = r"yolov(\d+)"
-    match = re.match(p, model_name.lower())
-    if match:
-        return f"YOLOv{match.group(1)}"
