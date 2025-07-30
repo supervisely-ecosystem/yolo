232cdf6bc687e5ad1b4be69be910009fe18bbdb1
vorozhkog <115161827+vorozhkog@users.noreply.github.com>
2025-07-30 18:23:33 +0100

    Update SDK version to `v6.73.408` (#11)
    * init
    
    * Update YOLO Docker image version to 1.0.9 in configuration files and scripts
    
    ---------
    
    Co-authored-by: cxnt <pavelbartsits@gmail.com>

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,7 +4,7 @@ PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 
 docker build --no-cache \
   -f "${SCRIPT_DIR}/Dockerfile.deploy" \
-  -t supervisely/yolo:1.0.8-deploy \
+  -t supervisely/yolo:1.0.9-deploy \
   "$PROJECT_ROOT"
 
-docker push supervisely/yolo:1.0.8-deploy
+docker push supervisely/yolo:1.0.9-deploy
--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/yolo:1.0.8 . && \
-docker push supervisely/yolo:1.0.8
+docker build -t supervisely/yolo:1.0.9 . && \
+docker push supervisely/yolo:1.0.9
