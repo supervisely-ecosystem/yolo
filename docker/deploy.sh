3ab6fd54d9c7ab5826198278d5b6bb562eb53daa
cxnt <pavelbartsits@gmail.com>
2025-08-22 14:24:47 +0000

    Update supervisely dependencies to version 6.73.421

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,7 +4,7 @@ PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 
 docker build --no-cache \
   -f "${SCRIPT_DIR}/Dockerfile.deploy" \
-  -t supervisely/yolo:1.0.11-deploy \
+  -t supervisely/yolo:1.0.12-deploy \
   "$PROJECT_ROOT"
 
-docker push supervisely/yolo:1.0.11-deploy
+docker push supervisely/yolo:1.0.12-deploy
